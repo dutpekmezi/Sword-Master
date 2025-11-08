c932b6530a7b6c95516100a6deb66393902bf71d
Dut Pekmezi <sirakkaya@gmail.com>
2025-11-08 12:52:41 +0300

    Update WeaponSelectionUI.cs

--- Assets/Scripts/UI/Game/Weapon/WeaponSelectionUI.cs
+++ Assets/Scripts/UI/Game/Weapon/WeaponSelectionUI.cs
@@ -1,3 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace dutpekmezi
+{
+    public class WeaponSelectionUI : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Transform parent;
+        [SerializeField] private WeaponCardUI weaponCardPrefab;
+        [SerializeField] private GameObject scnreenDim;
+
+        private List<WeaponCardUI> displayingWeaponCards = new List<WeaponCardUI>();
+        public void DisplayWeapons()
+        {
+            scnreenDim.SetActive(true);
+
+            var selectedWeapons = new List<WeaponData>();
+
+            selectedWeapons = WeaponSystem.Instance.GetRandomWeaponsData(2);
+
+            foreach (var weapon in selectedWeapons)
+            {
+                var instance = Instantiate(weaponCardPrefab, parent);
+                instance.Init(weapon);
+
+                displayingWeaponCards.Add(instance);
+            }
+        }
+
+        public void HideWeapons()
+        {
+            scnreenDim.SetActive(false);
+
+            if (displayingWeaponCards.Count > 0)
+            {
+                foreach (var weaponCard in displayingWeaponCards)
+                {
+                    Destroy(weaponCard.gameObject);
+                }
+
                 displayingWeaponCards.Clear();
             } 
         }
