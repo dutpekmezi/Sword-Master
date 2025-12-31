a44f59636a4335f4a37e3e66872bf09f00942c6a
Dut Pekmezi <118857762+dutpekmezi@users.noreply.github.com>
2025-12-31 15:44:09 +0300

    Add weapon health bar UI

--- Assets/Scripts/Weapon/WeaponBase.cs
+++ Assets/Scripts/Weapon/WeaponBase.cs
@@ -21,7 +21,11 @@ namespace dutpekmezi
         private bool canRotate = true;
 
         private float abilityCooldownTimer = 0f;
+        private float abilityCooldownDuration = 0f;
         private bool isAbilityReady = true;
+
+        public float AbilityCooldownRemaining => abilityCooldownTimer;
+        public float AbilityCooldownDuration => abilityCooldownDuration;
 
         public override void Initialize()
         {
@@ -37,16 +41,23 @@ namespace dutpekmezi
 
             isAbilityReady = true;
             abilityCooldownTimer = 0f;
+            abilityCooldownDuration = CalculateAbilityCooldownDuration();
 
             SignalBus.Get<StatSystem.OnStatSelected>().Subscribe(ApplySelectedModifier);
             SignalBus.Get<InputManager.OnAbilityButtonClick>().Subscribe(Ability);
+
+            SignalBus.Get<OnStatsChange>().Invoke(this);
         }
 
         public override void Tick()
         {
             Orbit();
             RotateSelf();
+            HandleAbilityCooldown();
+        }
 
+        private void HandleAbilityCooldown()
+        {
             if (!isAbilityReady)
             {
                 abilityCooldownTimer -= LogicTimer.FixedDelta;
@@ -54,16 +65,20 @@ namespace dutpekmezi
                 {
                     abilityCooldownTimer = 0f;
                     isAbilityReady = true;
+                    abilityCooldownDuration = CalculateAbilityCooldownDuration();
                 }
+            }
+            else
+            {
+                abilityCooldownDuration = CalculateAbilityCooldownDuration();
             }
         }
 
         protected void StartAbilityCooldown()
         {
-            float baseCooldown = GetStatValue(StatType.AbilityCooldown);
-            float cdr = CharacterSystem.Instance.GetCurrentCharacter().GetStatValue(StatType.CooldownReduction);
+            float finalCooldown = CalculateAbilityCooldownDuration();
 
-            float finalCooldown = baseCooldown * (1f - cdr);
+            abilityCooldownDuration = finalCooldown;
 
             if (finalCooldown > 0f)
             {
@@ -75,6 +90,14 @@ namespace dutpekmezi
                 isAbilityReady = true;
                 abilityCooldownTimer = 0f;
             }
+        }
+
+        private float CalculateAbilityCooldownDuration()
+        {
+            float baseCooldown = GetStatValue(StatType.AbilityCooldown);
+            float cdr = CharacterSystem.Instance.GetCurrentCharacter().GetStatValue(StatType.CooldownReduction);
+
+            return Mathf.Max(0f, baseCooldown * (1f - cdr));
         }
 
         protected virtual void Ability()
@@ -137,6 +160,34 @@ namespace dutpekmezi
                 LogicTimer.FixedDelta);
         }
 
+        protected override void ApplyModifier(StatModifier modifier)
+        {
+            base.ApplyModifier(modifier);
+
+            SignalBus.Get<OnStatsChange>().Invoke(this);
+        }
+
+        protected override void SetHealth(float amount)
+        {
+            base.SetHealth(amount);
+
+            SignalBus.Get<OnStatsChange>().Invoke(this);
+        }
+
+        public override void Gainlevel(int amount = 1)
+        {
+            base.Gainlevel(amount);
+
+            SignalBus.Get<OnStatsChange>().Invoke(this);
+        }
+
+        protected override void GainExp(float amount)
+        {
+            base.GainExp(amount);
+
+            SignalBus.Get<OnStatsChange>().Invoke(this);
+        }
+
         protected override void ApplySelectedModifier(StatModifier modifier)
         {
             if (modifier == null) return;
@@ -155,5 +206,7 @@ namespace dutpekmezi
             SignalBus.Get<StatSystem.OnStatSelected>().Unsubscribe(ApplySelectedModifier);
             SignalBus.Get<InputManager.OnAbilityButtonClick>().Unsubscribe(Ability);
         }
+
+        public class OnStatsChange : Signal<WeaponBase> { }
     }
 }
