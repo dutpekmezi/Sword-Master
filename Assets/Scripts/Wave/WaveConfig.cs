a8c083329fbc8009ec869d3d48aea16e9447f451
Atilla Akkaya <118857762+dutpekmezi@users.noreply.github.com>
2026-01-05 00:38:46 +0300

    Add minimum statue spawn spacing

--- Assets/Scripts/Wave/WaveConfig.cs
+++ Assets/Scripts/Wave/WaveConfig.cs
@@ -30,6 +30,7 @@ namespace dutpekmezi
         public int statuesPerWave;
         public float statueSpawnRadius;
         public float statueSpawnDeflection;
+        public float statueMinimumDistance;
         public float maxStatue;
         public float maxStatStatue;
         public float maxWeaponStatue;
