a9d367ed4d13563706627829bc0875cb62bc4ee7
Atilla Akkaya <118857762+dutpekmezi@users.noreply.github.com>
2026-01-13 14:07:49 +0300

    Add drop particle stat options

--- Assets/Scripts/Collectable/RandomDropParticle.cs
+++ Assets/Scripts/Collectable/RandomDropParticle.cs
@@ -4,20 +4,65 @@ namespace dutpekmezi
 {
     public class RandomDropParticle : Collectable
     {
+        [Header("Drop Settings")]
+        [SerializeField] private bool useRandomStat = true;
+        [SerializeField] private StatType selectedStat;
+        [SerializeField] [Min(0f)] private float scaleMultiplier = 1f;
+        [SerializeField] [Range(0f, 1f)] private float dropChance = 1f;
+
         [Header("Collect Settings")]
         [SerializeField] private float collectStartDelay = 0.1f;
         [SerializeField] private float collectFlySpeed = 3f;
 
         protected override void Init()
         {
+            if (!ShouldDrop())
+            {
+                Despawn();
+                return;
+            }
+
             base.Init();
 
             Collect();
         }
 
+        protected override StatModifier CreateCollectableModifier(float scaleFactor)
+        {
+            float scaledFactor = scaleFactor * scaleMultiplier;
+
+            if (useRandomStat)
+            {
+                return StatSystem.Instance.CreateRandomCollectableModifier(scaledFactor, this);
+            }
+
+            return StatSystem.Instance.CreateRandomModifier(selectedStat, scaledFactor, this);
+        }
+
         public void Collect()
         {
             Collect(collectStartDelay, collectFlySpeed);
+        }
+
+        private bool ShouldDrop()
+        {
+            if (dropChance >= 1f)
+            {
+                return true;
+            }
+
+            return Random.value <= dropChance;
+        }
+
+        private void Despawn()
+        {
+            if (Dutpekmezi.Services.PoolService.ObjectPoolManager.Instance != null)
+            {
+                Dutpekmezi.Services.PoolService.ObjectPoolManager.DeSpawn(gameObject);
+                return;
+            }
+
+            gameObject.SetActive(false);
         }
     }
 }
