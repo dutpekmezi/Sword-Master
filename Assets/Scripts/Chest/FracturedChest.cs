95ca21d9bc78a55675a5fbc6d5fb6efeab4557d4
Atilla Akkaya <118857762+dutpekmezi@users.noreply.github.com>
2026-01-21 13:24:29 +0300

    Reset fractured chest on respawn

--- Assets/Scripts/Chest/FracturedChest.cs
+++ Assets/Scripts/Chest/FracturedChest.cs
@@ -1,18 +1,69 @@
-using NUnit.Framework;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FracturedChest : MonoBehaviour
 {
     public Transform[] cellTransforms;
+
+    private Vector3[] initialLocalPositions;
+    private Quaternion[] initialLocalRotations;
+
+    private void Awake()
+    {
+        CacheInitialTransforms();
+    }
+
     public void Init()
     {
-        int i = 0;
+        CacheInitialTransforms();
+        ResetCells();
+    }
 
-        foreach (Transform transform in transform.GetComponentsInChildren<Transform>())
+    private void CacheInitialTransforms()
+    {
+        if (cellTransforms == null || cellTransforms.Length == 0)
         {
-            if (i < cellTransforms.Length) transform.position = cellTransforms[i].position;
-            i++;
+            return;
+        }
+
+        if (initialLocalPositions != null && initialLocalPositions.Length == cellTransforms.Length)
+        {
+            return;
+        }
+
+        initialLocalPositions = new Vector3[cellTransforms.Length];
+        initialLocalRotations = new Quaternion[cellTransforms.Length];
+
+        for (int i = 0; i < cellTransforms.Length; i++)
+        {
+            var cellTransform = cellTransforms[i];
+            if (cellTransform == null) continue;
+
+            initialLocalPositions[i] = cellTransform.localPosition;
+            initialLocalRotations[i] = cellTransform.localRotation;
+        }
+    }
+
+    private void ResetCells()
+    {
+        if (cellTransforms == null || cellTransforms.Length == 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < cellTransforms.Length; i++)
+        {
+            var cellTransform = cellTransforms[i];
+            if (cellTransform == null) continue;
+
+            cellTransform.localPosition = initialLocalPositions[i];
+            cellTransform.localRotation = initialLocalRotations[i];
+
+            if (cellTransform.TryGetComponent<Rigidbody2D>(out var body))
+            {
+                body.velocity = Vector2.zero;
+                body.angularVelocity = 0f;
+                body.Sleep();
+            }
         }
     }
 }
